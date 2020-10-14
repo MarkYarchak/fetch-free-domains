582c1c83f64b00a4ea7e5c5f41c4694aefaa293c
Mark Yarchak <markyarchak.work@gmail.com>
2020-10-14 11:20:28 +0300

    revert write-file changes

--- write-file.js
+++ write-file.js
@@ -4,14 +4,16 @@ const fs = require('fs');
 // create folders recursive before write file
 function writeDomainsFileRecursive(filename, content) {
   const folders = filename.split(path.sep).slice(0, -1);
-  folders.reduce((last, folder) => {
-    const folderPath = last ? last + path.sep + folder : folder;
-    if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath);
-    }
-    return folderPath;
-  });
-
+  if (folders.length) {
+    // create folder path if it doesn't exist
+    folders.reduce((last, folder) => {
+      const folderPath = last ? last + path.sep + folder : folder;
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath);
+      }
+      return folderPath;
+    });
+  }
   fs.writeFileSync(filename, content, { encoding: 'utf8' });
 }
 
